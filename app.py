d6950b42faee61571877d4c92aa41a366ae33f71
Sogo Ogundowole <dowolebolu@gmail.com>
2020-01-22 11:26:58 +0100

    removed task constraint

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask, request, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///exquisapp'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB_URI')
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
@@ -12,7 +18,7 @@ migrate = Migrate(app, db)
 class Task(db.Model):
     __tablename__ = 'tasks'
     id = db.Column(db.Integer, primary_key=True)
-    task = db.Column(db.String(150))
+    task = db.Column(db.String())
     done = db.Column(db.Boolean)
 
 
@@ -27,10 +33,13 @@ def index():
 
 @app.route('/add', methods=['POST'])
 def add():
-    todo = Task(task=request.form['todoitem'], done=False)
-    db.session.add(todo)
-    db.session.commit()
-    db.session.close()
+    if request.form['todoitem'] == '':
+        pass
+    else:
+        todo = Task(task=request.form['todoitem'], done=False)
+        db.session.add(todo)
+        db.session.commit()
+        db.session.close()
 
     return redirect(url_for('index'))
 
@@ -49,7 +58,6 @@ def update(id):
 @app.route('/delete/<id>')
 def delete(id):
     Task.query.filter_by(id=int(id)).delete()
-    # task.delete()
     db.session.commit()
     db.session.close()
 
@@ -76,4 +84,4 @@ def complete(id):
 db.create_all()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
