dd54a7b9c4f11f8b4644c3f554268f21549fe70b
Sogo Ogundowole <dowolebolu@gmail.com>
2020-01-22 10:37:16 +0100

    added requirement.txt

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
